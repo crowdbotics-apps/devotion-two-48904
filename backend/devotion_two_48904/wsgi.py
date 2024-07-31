68c8e6b237c1ec7b51ad72be5ed090f4c5512ef4
Crowdbotics <team@crowdbotics.com>
2024-07-31 04:24:46 +0000

    Initial commit

---
+++ backend/devotion_two_48904/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for devotion_two_48904 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "devotion_two_48904.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "devotion-two-48904",
+  url: "https://devotion-two-48904.botics.co",
+  api: "https://devotion-two-48904.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
